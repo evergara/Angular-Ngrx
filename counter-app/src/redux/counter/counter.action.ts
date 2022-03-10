0b49ecf3200ea118655cc052bb47f1393eecd098
Emerson Vergara <ee-web@hotmail.com>
2022-03-09 19:07:31 -0500

    Creamos una clase por cada accion y exportamos esto nos permite no estar pendiente de las contantes de typos de acciones

--- counter-app/src/redux/counter/counter.action.ts
+++ counter-app/src/redux/counter/counter.action.ts
@@ -1,7 +1,24 @@
 import { Action} from "@ngrx/store";
 import { DECREMENT, INCREMNET, PLUS, RESET } from "./counter.action.constants";
 
-export interface CounterAction extends Action {
-    payload: number;
+
+export class IncrementAction implements Action {
+    readonly type = INCREMNET;
 }
 
+export class DecrementAction implements Action {
+    readonly type = DECREMENT;
+}
+
+export class ResetAction implements Action {
+    readonly type = RESET;
+}
+
+export class PlustAction implements Action {
+    readonly type = PLUS;
+
+    constructor(public payload: number){}
+}
+
+
+export type AllCounterActtion = IncrementAction | DecrementAction | ResetAction | PlustAction;
