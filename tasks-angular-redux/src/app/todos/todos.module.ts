b052f74ae1e00cd34622418abe346d28d4e289b9
Emerson Vergara <ee-web@hotmail.com>
2022-03-12 07:19:07 -0500

    adecauacin de componentes

--- tasks-angular-redux/src/app/todos/todos.module.ts
+++ tasks-angular-redux/src/app/todos/todos.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodosRoutingModule } from './todos-routing.module';
+import { TodosComponent } from './todos.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodosComponent,
+    TodoComponent,
+    TodoListComponent
+  ],
   imports: [
     CommonModule,
     TodosRoutingModule
